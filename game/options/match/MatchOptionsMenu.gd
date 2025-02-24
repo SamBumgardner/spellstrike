bbb442a6f275d0dbda47e92e553865d2357a70fa
Sam Bumgardner <Sambumgardner1@gmail.com>
2025-02-24 12:36:41 -0600

    add very basic support for multiple CPU styles.
    Not very extensible, but that's okay. Mostly just a basic proof of concept

--- game/options/match/MatchOptionsMenu.gd
+++ game/options/match/MatchOptionsMenu.gd
@@ -19,7 +19,10 @@ func get_match_options() -> MatchOptions:
     )
 
 func get_input_retrievers() -> Array:
-    var p1_mapper = RandomCpuInputRetriever.new() if $"%P1_CPU".button_pressed else InputRetriever.new(InputMappingManager.p1_input_mapping)
-    var p2_mapper = RandomCpuInputRetriever.new() if $"%P2_CPU".button_pressed else InputRetriever.new(InputMappingManager.p2_input_mapping)
+    var p1_cpu = SimpleCpuRetriever.new() if $"%CpuTypesP1".selected == 0 else RandomCpuInputRetriever.new()
+    var p1_mapper = p1_cpu if $"%P1_CPU".button_pressed else InputRetriever.new(InputMappingManager.p1_input_mapping)
+    
+    var p2_cpu = SimpleCpuRetriever.new() if $"%CpuTypesP2".selected == 0 else RandomCpuInputRetriever.new()
+    var p2_mapper = p2_cpu if $"%P2_CPU".button_pressed else InputRetriever.new(InputMappingManager.p2_input_mapping)
         
     return [p1_mapper, p2_mapper]
