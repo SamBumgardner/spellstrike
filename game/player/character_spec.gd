0703f6a47739fb3cbb41d559d828becf089b7126
Sam Bumgardner <Sambumgardner1@gmail.com>
2025-03-14 19:50:15 -0500

    expand contents of character spec

--- game/player/character_spec.gd
+++ game/player/character_spec.gd
@@ -1,8 +1,22 @@
 class_name CharacterSpec extends Resource
 
-var name: String
-var victory_portrait: Texture
-var defeat_portrait: Texture
+@export var name: String
+@export var victory_portrait: Texture
+@export var defeat_portrait: Texture
+
+@export var animation_sprite: Texture
+@export var special_icon: Texture
+
+@export var walk_speed: int = 5
+@export var back_walk_speed: int = 3
+@export var walk_accel: int = 5
+@export var walk_drag: int = 2
+@export var width: int = 128
+@export var pushbox_width: int = 64
+
+@export var max_hp: int = 150
+@export var special_uses: int = 5
+@export var states: Array[FsmState]
 
 func _init() -> void:
     name = "placeholder_name"
